25423dac4718115804644a889152e9a7e07d6279
Mohit <mohitv@exzeo.com>
2015-09-12 10:39:11 +0530

    Some changes

--- app/src/com/land/farm/helper/ParseJson.java
+++ app/src/com/land/farm/helper/ParseJson.java
@@ -1,6 +1,7 @@
 package com.land.farm.helper;
 
 import com.land.farm.models.JobSeeker;
+import com.land.farm.models.Location;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -28,5 +29,34 @@ public class ParseJson {
         }
 
         return jobseekers;
+    }
+
+
+    public ArrayList<Location> getLocations(String jsonString) {
+
+        ArrayList<Location> locations = new ArrayList<Location>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONArray values = jsonArray.getJSONArray(i);
+
+                Location location = new Location();
+                location.id = values.getString(0);
+                location.value = values.getString(1);
+
+                locations.add(location);
+
+            }
+
+        } catch (Exception exception) {
+            //Todo : Handle exception
+
+        }
+
+        return locations;
+
     }
 }
