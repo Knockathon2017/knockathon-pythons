8f1f308b00b5eb27c7c8e4c2090d7bb1a07b9e4d
Chandan Rai <chandanr@exzeo.com>
2015-09-12 13:43:12 +0530

    api code commit

--- api/KnockAPI/Models/WorkProvider.cs
+++ api/KnockAPI/Models/WorkProvider.cs
@@ -4,7 +4,6 @@ namespace KnockAPI.Models
     public class WorkProvider : User
     {
         public long WorkProviderId { get; set; }
-        public long UserId { get; set; }
         public int RequiredWorkforce { get; set; }
         
     }
